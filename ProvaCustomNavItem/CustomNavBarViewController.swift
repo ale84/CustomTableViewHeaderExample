ada41e15ce3432003bae1a8f9a0c6faeea365371
Alessio Orlando <a.orlando@onetech.it>
2016-07-13 15:09:23 +0200

    handle appearance/disappearance of title view while scrolling

--- ProvaCustomNavItem/CustomNavBarViewController.swift
+++ ProvaCustomNavItem/CustomNavBarViewController.swift
@@ -15,63 +15,55 @@ class CustomNavBarViewController: UIViewController {
     @IBOutlet weak var customNavigationBar: UINavigationBar!
     var startLocation: CGPoint? = nil
     var headerViewOriginalHeight: CGFloat = 0
+    var titleLabel: UILabel = UILabel()
 
     override func viewDidLoad() {
         super.viewDidLoad()
         self.customNavigationBar.setBackgroundImage(UIImage(), forBarMetrics: .Default)
         self.customNavigationBar.shadowImage = UIImage()
         self.customNavigationBar.translucent = true
+        
         let item = UINavigationItem(title: "")
+        titleLabel.text = "titolo"
+        titleLabel.font = UIFont.boldSystemFontOfSize(17)
+        titleLabel.sizeToFit()
+        item.titleView = titleLabel
+        
         customNavigationBar.pushNavigationItem(item, animated: true)
         
         headerView.layoutIfNeeded()
         headerViewOriginalHeight = headerView.frame.size.height
     }
     
+    override func viewDidAppear(animated: Bool) {
+        super.viewDidAppear(animated)
+        titleLabel.alpha = 0
+    }
+    
     @IBAction func handlePan(sender: UIPanGestureRecognizer) {
         let translation = sender.translationInView(self.view)
         sender.setTranslation(CGPointZero, inView: self.view)
         
-//        if sender.state == .Ended {
-//            let fadeTextAnimation = CATransition()
-//            fadeTextAnimation.duration = 0.5
-//            fadeTextAnimation.type = kCATransitionFade
-//            
-//            customNavigationBar.layer.addAnimation(fadeTextAnimation, forKey: "fadeText")
-//            customNavigationBar.topItem?.title = arc4random() % 2 == 0 ? "test 123" : "asdasd"
-//        }
-        
-        let newConstant = verticalSpaceConstraint.constant + translation.y
+        var newConstant = verticalSpaceConstraint.constant + translation.y
+        let alphaChange = translation.y/(headerViewOriginalHeight/2)
 
         switch newConstant {
-        case 0..<headerViewOriginalHeight/2:
+        case 1..<headerViewOriginalHeight/2:
             headerView.alpha = 0
+            titleLabel.alpha = titleLabel.alpha - alphaChange
         case headerViewOriginalHeight/2...headerViewOriginalHeight:
-            let alphaChange = translation.y/(headerViewOriginalHeight/2)
-            var newAlpha = headerView.alpha + alphaChange
-            if newAlpha <= 0 {
-                newAlpha = 0
-            }
-            if newAlpha > 1 {
-                newAlpha = 1
-            }
-            headerView.alpha = newAlpha
+            titleLabel.alpha = 0
+            headerView.alpha = headerView.alpha + alphaChange
         case _ where newConstant > headerViewOriginalHeight:
+            newConstant = headerViewOriginalHeight
             headerView.alpha = 1
+        case _ where newConstant <= 0:
+            newConstant = 1
+            titleLabel.alpha = 1
         default:
             break
         }
-
-        if newConstant < 0 {
-            verticalSpaceConstraint.constant = 0
-        }
-        else if newConstant > headerViewOriginalHeight {
-            verticalSpaceConstraint.constant = headerViewOriginalHeight
-        }
-        else {
-            verticalSpaceConstraint.constant = newConstant
-        }
-        view.layoutIfNeeded()
+        verticalSpaceConstraint.constant = newConstant
     }
 }
 
