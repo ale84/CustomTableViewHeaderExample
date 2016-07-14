1d90ced9446f09ef27de9629badfe99cd14fb3a2
Alessio Orlando <a.orlando@onetech.it>
2016-07-14 10:48:55 +0200

    refactoring using scrollview delegate instead of pan gesture recognizer

--- ProvaCustomNavItem/CustomNavBarViewController.swift
+++ ProvaCustomNavItem/CustomNavBarViewController.swift
@@ -14,9 +14,9 @@ class CustomNavBarViewController: UIViewController {
     @IBOutlet weak var headerView: UIView!
     @IBOutlet var verticalSpaceConstraint: NSLayoutConstraint!
     @IBOutlet weak var customNavigationBar: UINavigationBar!
-    var startLocation: CGPoint? = nil
     var headerViewOriginalHeight: CGFloat = 0
     var titleLabel: UILabel = UILabel()
+    var lastVerticalOffset: CGFloat = 0
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -38,48 +38,12 @@ class CustomNavBarViewController: UIViewController {
         
         tableView.contentInset = UIEdgeInsets(top: headerViewOriginalHeight, left: 0, bottom: 0, right: 0)
         tableView.scrollIndicatorInsets = tableView.contentInset
+        lastVerticalOffset = tableView.contentOffset.y
     }
     
     override func viewDidAppear(animated: Bool) {
         super.viewDidAppear(animated)
         titleLabel.alpha = 0
-    }
-    
-    @IBAction func handlePan(sender: UIPanGestureRecognizer) {
-//        let tableViewOffsetY = tableView.contentOffset.y + tableView.contentInset.top
-//        guard tableViewOffsetY >= 0 && tableViewOffsetY <= headerViewOriginalHeight else {
-//            return
-//        }
-        
-        let translation = sender.translationInView(self.view)
-        sender.setTranslation(CGPointZero, inView: self.view)
-        
-        var newConstant = verticalSpaceConstraint.constant + translation.y
-        let alphaChange = translation.y/(headerViewOriginalHeight/2)
-
-        switch newConstant {
-        case 1..<headerViewOriginalHeight/2:
-            headerView.alpha = 0
-            titleLabel.alpha = titleLabel.alpha - alphaChange
-        case headerViewOriginalHeight/2...headerViewOriginalHeight:
-            titleLabel.alpha = 0
-            headerView.alpha = headerView.alpha + alphaChange
-        case _ where newConstant > headerViewOriginalHeight:
-            newConstant = headerViewOriginalHeight
-            headerView.alpha = 1
-        case _ where newConstant <= 0:
-            newConstant = 1
-            titleLabel.alpha = 1
-        default:
-            break
-        }
-        verticalSpaceConstraint.constant = newConstant
-    }
-}
-
-extension CustomNavBarViewController: UIGestureRecognizerDelegate {
-    func gestureRecognizer(gestureRecognizer: UIGestureRecognizer, shouldRecognizeSimultaneouslyWithGestureRecognizer otherGestureRecognizer: UIGestureRecognizer) -> Bool {
-        return true
     }
 }
 
@@ -96,5 +60,32 @@ extension CustomNavBarViewController: UITableViewDelegate, UITableViewDataSource
         let cell = tableView.dequeueReusableCellWithIdentifier("cell")!
         cell.textLabel?.text = "\(indexPath.row)"
         return cell
+    }
+    
+    func scrollViewDidScroll(scrollView: UIScrollView) {
+        let verticalOffset = scrollView.contentOffset.y
+        let scrollAmount = verticalOffset - lastVerticalOffset
+        lastVerticalOffset = verticalOffset
+        
+        var newConstant = -verticalOffset
+        let alphaVariation = scrollAmount/(headerViewOriginalHeight/2)
+        
+        switch newConstant {
+        case _ where newConstant <= 0:
+            newConstant = 1
+            titleLabel.alpha = 1
+        case 1..<headerViewOriginalHeight/2:
+            headerView.alpha = 0
+            titleLabel.alpha = titleLabel.alpha + alphaVariation
+        case headerViewOriginalHeight/2...headerViewOriginalHeight:
+            titleLabel.alpha = 0
+            headerView.alpha = headerView.alpha - alphaVariation
+        case _ where newConstant > headerViewOriginalHeight:
+            newConstant = headerViewOriginalHeight
+            headerView.alpha = 1
+        default:
+            break
+        }
+        verticalSpaceConstraint.constant = newConstant
     }
 }
--- ProvaCustomNavItem/CustomTableViewController.swift
+++ ProvaCustomNavItem/CustomTableViewController.swift
@@ -11,9 +11,13 @@ import UIKit
 class CustomTableViewController: UITableViewController {
     
     override func viewDidLoad() {
-        self.navigationController?.navigationBar.setBackgroundImage(UIImage(), forBarMetrics: .Default)
-        self.navigationController?.navigationBar.shadowImage = UIImage()
-        self.navigationController?.navigationBar.translucent = true
-        tableView.contentInset = UIEdgeInsets(top: 20, left: 0, bottom: 0, right: 0)
+//        self.navigationController?.navigationBar.setBackgroundImage(UIImage(), forBarMetrics: .Default)
+//        self.navigationController?.navigationBar.shadowImage = UIImage()
+//        self.navigationController?.navigationBar.translucent = true
+//        tableView.contentInset = UIEdgeInsets(top: 20, left: 0, bottom: 0, right: 0)
+        let headerView = UIView(frame: CGRect(x: 0, y: 0, width: view.bounds.size.width, height: 250))
+            headerView.backgroundColor = UIColor .redColor()
+        tableView.addSubview(headerView)
+        //tableView.contentInset = UIEdgeInsets(top: tableView.contentInset.top + 250, left: 0, bottom: 0, right: 0)
     }
 }
